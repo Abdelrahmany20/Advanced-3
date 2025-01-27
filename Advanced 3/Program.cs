e1ca244b2d490b7b4a86712ba2aea16bd9ce062c
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2025-01-27 22:19:55 +0200

    P2-Q02

--- Advanced 3/Program.cs
+++ Advanced 3/Program.cs
@@ -33,6 +33,36 @@ namespace Advanced_3
 
             //foreach (int nums in numbers)
             //{
+            //    if (count.ContainsKey(nums))
+            //        count[nums] = (int)count[nums] + 1;
+
+            //    else
+            //        count[nums] = 1;
+            //}
+
+
+            //foreach (DictionaryEntry item in count)
+            //{
+            //    Console.WriteLine($"key : {item.Key} , value : {item.Value}");
+
+
+
+            //}
+
+
+            #endregion
+
+
+            #region  Q02
+
+
+            //int[] numbers = { 1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9 };
+
+
+            //Hashtable count = new Hashtable();
+
+            //foreach (int nums in numbers)
+            //{
                 //if (count.ContainsKey(nums))
                     //count[nums] = (int)count[nums] + 1;
 
@@ -41,16 +71,56 @@ namespace Advanced_3
             //}
 
 
+            //int maxvalue=int.MinValue;
+            //int maxkey=default;
+
+            //foreach (DictionaryEntry item in count)
+            //{
+                //if ((int)item.Value > maxvalue)
+                //{
+                    //maxvalue = (int)item.Value;
+                    //maxkey = (int)item.Key;
+                //}
+            //}
+
+
             //foreach (DictionaryEntry item in count)
             //{
                 //Console.WriteLine($"key : {item.Key} , value : {item.Value}");
 
-              
+
 
             //}
 
+            //Console.WriteLine($"max key: {maxkey} , max value: {maxvalue}");
 
             #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             #endregion
 
