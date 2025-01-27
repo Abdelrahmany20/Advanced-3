a65955574bf549c864effbacbb395c5ccda1456d
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2025-01-27 21:57:49 +0200

    P2-Q01

--- Advanced 3/Program.cs
+++ Advanced 3/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Advanced_3
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace Advanced_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Part 1
+
+            //Write a Report about all Collections Given in the Session and Compare[Structure, Time Complexity,
+            //and Business Case With Implemented Examples in Separates Project at the Same Solution]
+
+
+
+
+            #endregion
+
+
+
+            #region Part 2
+
+
+
+            #region Q01
+            //int[] numbers = { 1, 2, 3, 4, 4, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9 };
+
+
+            //Hashtable count = new Hashtable();
+
+            //foreach (int nums in numbers)
+            //{
+                //if (count.ContainsKey(nums))
+                    //count[nums] = (int)count[nums] + 1;
+
+                //else
+                    //count[nums] = 1;
+            //}
+
+
+            //foreach (DictionaryEntry item in count)
+            //{
+                //Console.WriteLine($"key : {item.Key} , value : {item.Value}");
+
+              
+
+            //}
+
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
